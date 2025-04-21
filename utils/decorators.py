02a9cbb7c24d3e0942af4d3f43d9c20924fd3069
Sakthisudarsen Jayakumar <166731678+Sakthisudarsh1206@users.noreply.github.com>
2025-04-21 20:32:11 +0530

    Update decorators.py

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,6 +1,6 @@
+# utils/decorators.py
 import functools
 from loguru import logger
-
 def logger_wraps(*, entry=True, exit=True, level="DEBUG"):
     def wrapper(func):
         name = func.__name__
@@ -8,7 +8,7 @@ def logger_wraps(*, entry=True, exit=True, level="DEBUG"):
         def wrapped(*args, **kwargs):
             logger_ = logger.opt(depth=1)
             if entry:
-                logger_.log(level, "Entering '{}' (args={}, kwargs={})", name, args, kwargs)
+                logger_.log(level,"Entering '{}' (args={}, kwargs={})",name, args, kwargs)
             result = func(*args, **kwargs)
             if exit:
                 logger_.log(level, "Exiting '{}' (result={})", name, result)
